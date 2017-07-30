71f5d9d17b5178553585dae40bc95d55315bd7e5
Jason Bandlow <jbandlow@gmail.com>
2017-07-30 15:09:42 -0700

    Fix typo in tf_start.sh

--- scripts/tf_start.sh
+++ scripts/tf_start.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-# `source` this file to properly export TENSORFLOW_CONTAINER
+# `source` this file to properly export TF_CONTAINER
 LOGDIR=/tmp/tensorflow_logs
 TF_CONTAINER=`docker run -d -p 8889:8888 -p 6006:6006 -v $PWD:/home/jovyan/work jupyter/tensorflow-notebook start-notebook.sh --NotebookApp.token=''`
-docker exec -d $TENSORFLOW_CONTAINER tensorboard --logdir=$LOGDIR
+docker exec -d $TF_CONTAINER tensorboard --logdir=$LOGDIR
 export TF_CONTAINER
 echo "Tensorflow notebook server is running at http://localhost:8889"
 echo "Tensorboard is running from docker directory $LOGDIR at http://localhost:6006"
